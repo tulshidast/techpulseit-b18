dfd50537c60550a0086b8a66df41f9cae7d51bc1
tulshidast <tulsidast77@gmail.com>
2026-02-21 08:39:26 +0530

    Resolved review comment

--- src/test/java/pages/AddToCart.java
+++ src/test/java/pages/AddToCart.java
@@ -2,6 +2,10 @@ package pages;
 
 import base.BasePage;
 
+/**
+ * This class is used to write locators and related method from add to cart
+ * page.
+ */
 public class AddToCart extends BasePage {
 
 }
